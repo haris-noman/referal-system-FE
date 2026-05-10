6df5283639b24a34c36e03678632aebdca1f8277
haris-noman <harisnoman.dh@gmail.com>
2026-05-10 21:48:44 +0500

    add link

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target:
+            env.VITE_API_BASE_URL ||
+            "https://referal-system-django-production.up.railway.app",
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
+  };
 });
